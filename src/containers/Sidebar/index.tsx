1bbe815e83b91a1c62937480034095e4d2edab5c
Mauricio de Mello <mau-ideias@hotmail.com>
2023-10-02 23:11:44 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,30 @@
 import Title from '../../components/Title'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+
+import { Descricao, BotaoTema, SidebarContainer, LinkedIn } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/KwizatzHaderach.png"></img>
-    <Title fontSize={20}>Maurício Mello</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Maurício Mello</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @KwizatzHaderach
+      </Paragrafo>
+      <LinkedIn>
+        <a href="https://www.linkedin.com/in/maur%C3%ADcio-de-mello-091300223/">
+          <img
+            src="https://media.licdn.com/dms/image/C560BAQHaVYd13rRz3A/company-logo_200_200/0/1638831589865?e=1704326400&v=beta&t=LrBN5xYmIFYeo6GOg4dHOIaLBUUk0LHDuMPi_rGvngM"
+            alt="LinkedIN"
+          />
+        </a>
+      </LinkedIn>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Web
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
